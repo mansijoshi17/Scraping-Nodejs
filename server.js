e0e9f81fc53aeb0d087c4d01b6c5f342993f41dc
Mansi Joshi <mansijoshi1777@gmail.com>
2020-04-07 14:22:07 +0530

    Added comments and clean the code

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ var jsonarry = [];
 
 app.get('/scrape', function (req, res) {
 
+    // --------------------------- COINTOPPER-SCRAPING----------------------------------------//
+
     url = 'https://cointopper.com/';
 
     request(url, function (error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
-            // --------------------------- COINTOPPER-SCRAPING----------------------------------------//
+
 
             $("#MyTable > tbody > tr").each((index, element) => {
                 var imgtd = $(element).find('td')[1];
@@ -35,6 +37,24 @@ app.get('/scrape', function (req, res) {
 
 
         }
+
+        // To write to the system we will use the built in 'fs' library.
+        // In this example we will pass 3 parameters to the writeFile function
+        // Parameter 1 :  output.json - this is what the created filename will be called
+        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+        fs.writeFile('output.json', JSON.stringify(jsonarry, null, 4), function (err) {
+
+            console.log('File successfully written! - Check your project directory for the output.json file');
+
+        })
+
+
+        // --------------------------- COINTOPPER-SCRAPING----------------------------------------//
+
+
+        //----------------------------- MYSQL connection and add data --------------------------//
 
         // Configure MySQL connection
         var connection = mysql.createConnection({
@@ -63,7 +83,7 @@ app.get('/scrape', function (req, res) {
         })
 
 
-        var sql = "INSERT INTO coindetails (id, name, marketValue, mPrice, coins, imgUrl, mCap) VALUES ?" ;
+        var sql = "INSERT INTO coindetails (id, name, marketValue, mPrice, coins, imgUrl, mCap) VALUES ?";
         // Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
         connection.query(sql, [values], function (err, result) {
             if (err) {
@@ -73,23 +93,9 @@ app.get('/scrape', function (req, res) {
             else {
                 res.send('Success! check phpmyAdmin');
             }
-            // console.log(query.sql);
         });
 
-        // To write to the system we will use the built in 'fs' library.
-        // In this example we will pass 3 parameters to the writeFile function
-        // Parameter 1 :  output.json - this is what the created filename will be called
-        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-        // Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-        fs.writeFile('output.json', JSON.stringify(jsonarry, null, 4), function (err) {
-
-            console.log('File successfully written! - Check your project directory for the output.json file');
-
-        })
-
-        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-        // res.send('Check your console!')
+        //----------------------------- MYSQL connection and add data --------------------------// 
 
     });
 })
